3341f0fcb46fde63e08e45821eb61e60eac0aaeb
Joysclick <alden-990@163.com>
2016-10-02 17:12:18 +0800

    Initial commit

---
+++ src/com/joysclick/test/basic/StringTest.java
@@ -0,0 +1,14 @@
+package com.joysclick.test.basic;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by joysclick on 2016/10/2.
+ */
+public class StringTest {
+
+    @Test
+    public void HelloWorld(){
+        System.out.println("Hello World!");
+    }
+}
