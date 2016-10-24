6ee6d97797543e9cd81e58a4dfc145f1f7c0276d
ubuntu <alden-990@163.com>
2016-10-23 20:06:44 -0700

    Add HashSetTest

--- src/com/joysclick/test/basic/CollectionTest.java
+++ src/com/joysclick/test/basic/CollectionTest.java
@@ -21,6 +21,26 @@ public class CollectionTest {
 
     }
 
+    @Test
+    public void HashSetTest(){
+        Set<String> words = new HashSet<String>();
+        long totalTime = 0;
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String word = in.next();
+            long callTime = System.currentTimeMillis();
+            words.add(word);
+            totalTime += callTime;
+        }
+
+        Iterator<String> iter = words.iterator();
+        for(int i=1; i<=20; i++){
+            System.out.println(iter.next());
+        }
+        System.out.println("...");
+        System.out.println(words.size() + "distinct words. " + totalTime + "millionseconds.");
+    }
+
 
 
 
